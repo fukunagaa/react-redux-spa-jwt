51e5ca39f62154ca6ef5e656f1b32c2864e1029e
fukunagaa <k.k.0627ku@gmail.com>
2020-08-23 17:55:49 +0900

    dummyデータを追加

---
+++ src/dummies/articles.js
@@ -0,0 +1,62 @@
+[
+  {
+    author: "aaa",
+    title: "人気のソフトクリーム投稿",
+    urlToImage: "https://picsum.photos/id/10/200/200",
+    publishedAt: "2019-05-22T06:14:23Z",
+  },
+  {
+    author: "bbb",
+    title: "まさかの犯人　ドラマ完結!?",
+    urlToImage: "https://picsum.photos/id/13/200/200",
+    publishedAt: "2019-05-22T05:42:23Z",
+  },
+  {
+    author: "ccc",
+    title: "お化け屋敷　本日オープン",
+    urlToImage: "https://picsum.photos/id/17/200/200",
+    publishedAt: "2019-05-21T15:33:23Z",
+  },
+  {
+    author: "ddd",
+    title: "中学受験不安。",
+    urlToImage: "https://picsum.photos/id/20/200/200",
+    publishedAt: "2019-05-21T14:02:23Z",
+  },
+  {
+    author: "eee",
+    title: "帰省ラッシュ　満員電車で帰省",
+    urlToImage: "https://picsum.photos/id/22/200/200",
+    publishedAt: "2019-05-21T11:55:23Z",
+  },
+  {
+    author: "fff",
+    title: "100人のインタビューから見えたビジネスマンの真相",
+    urlToImage: "https://picsum.photos/id/24/200/200",
+    publishedAt: "2019-05-21T11:42:23Z",
+  },
+  {
+    author: "ggg",
+    title: "ハリウッドスターの愛用する「一流ガジェット」たち",
+    urlToImage: "https://picsum.photos/id/26/200/200",
+    publishedAt: "2019-05-21T10:32:23Z",
+  },
+  {
+    author: "hhh",
+    title: "70歳登山家がエベレスト登頂に挑戦。地元の期待膨らむ。",
+    urlToImage: "https://picsum.photos/id/29/200/200",
+    publishedAt: "2019-05-21T10:02:23Z",
+  },
+  {
+    author: "iii",
+    title: "銀座に新たなコンセプトのコーヒーショップがオープン",
+    urlToImage: "https://picsum.photos/id/30/200/200",
+    publishedAt: "2019-05-19T09:02:23Z",
+  },
+  {
+    author: "aaa",
+    title: "気になる週末の天気。関東地方の天気は概ね晴れ。",
+    urlToImage: "https://picsum.photos/id/38/200/200",
+    publishedAt: "2019-05-19T09:01:23Z",
+  },
+];
